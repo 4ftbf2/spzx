c2905e9f9ef25f47a728e001a9dfac75a7865d52
hsxm <mzc5840@gmail.com>
2025-05-06 18:59:13 +0800

    p11，登录测试

--- spzx-manager/src/main/java/com/xju/spzx/manager/ManagerApplication.java
+++ spzx-manager/src/main/java/com/xju/spzx/manager/ManagerApplication.java
@@ -2,8 +2,10 @@ package com.xju.spzx.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.xju.spzx")
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class, args);
